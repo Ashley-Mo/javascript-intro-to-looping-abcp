fa8b18e87c9f17f08cfc064d7ee39ea9ef273f99
Aswathylal Mohanlal <lal.aswathy@gmail.com>
2018-01-20 16:50:56 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -10,9 +10,13 @@ function whileLoop(n){
   }return 'done';
 }
 
+function maybeTrue() {
+  return Math.random() >= 0.5 // Returns a random number between 0 (inclusive) and 1 (exclusive)
+}
+
 function doWhileLoop(array){
   do{
     
   }
-  while(array);
+  while(array.length);
 }
